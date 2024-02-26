d4abec758cdae2e4adc30b2c9bdc6fb514794ba4
Alfredo Serrano Figueroa <86935645+A1fred00-datascience@users.noreply.github.com>
2024-02-26 17:25:49 -0500

    Updated Model Training and feature engineering

--- WHOOP_Project.py
+++ WHOOP_Project.py
@@ -6,6 +6,11 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.experimental import enable_hist_gradient_boosting  # noqa
+from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import GridSearchCV
+
 
 
 #%% Reading csv files and transforming them into dataframes
@@ -62,24 +67,41 @@ plt.show()
 X = merged_df.select_dtypes(include=[np.number]).drop('Recovery score %', axis=1)
 y = merged_df['Recovery score %']
 
-#%% Feature Scaling
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+#%% Splitting the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-#%% Splitting the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+#%% Initialize the model
+hgb = HistGradientBoostingRegressor()
 
-#%% Model Building
-model = RandomForestRegressor(random_state=42)
-model.fit(X_train, y_train)
+#%% Train the model
+hgb.fit(X_train, y_train)
 
-#%% Predictions and Evaluation
-y_pred = model.predict(X_test)
-print(f"Mean Squared Error: {mean_squared_error(y_test, y_pred)}")
-print(f"R-squared: {r2_score(y_test, y_pred)}")
+#%% Predictions generations
+y_pred = hgb.predict(X_test)
 
-#%% Displaying feature importances
-feature_importances = pd.Series(model.feature_importances_, index=X.columns)
-feature_importances.nlargest(10).plot(kind='barh')
-plt.title('Top 10 Important Features')
-plt.show()
+#%% Model Evaluation
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+#%% Evaluation Printing
+print(f"Mean Absolute Error (MAE): {mae}")
+print(f"Mean Squared Error (MSE): {mse}")
+print(f"R-squared (R²): {r2}")
+
+
+#%% Model Tuning
+param_grid = {
+    'max_depth': [3, 5, 10],
+    'learning_rate': [0.01, 0.1, 0.2],
+    'min_samples_leaf': [20, 40, 60],
+    # Add other parameters here
+}
+grid_search = GridSearchCV(hgb, param_grid, cv=5, scoring='r2')
+grid_search.fit(X_train, y_train)
+
+print("Best parameters:", grid_search.best_params_)
+
+#%% Exporting the merged dataset
+merged_df.to_csv('merged_dataset_for_tableau.csv', index=False)
+
